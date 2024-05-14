8eb1de9cdb364c38ae77c5c928497fb796839da5
farhanmoula21 <fmoula96@gmail.com>
2024-05-14 15:20:10 +0530

    made an ui

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorBtn = document.querySelector(".scissor");
+const txt = document.querySelector(".textbox");
 function getComputerChoice(){
     const arr = ["rock", "paper", "scissor"];
     const opt =arr[Math.floor(Math.random() *3)];
     return opt;
 }
-
 const computerSelection=  getComputerChoice();
+let pChoice = "";
+rockBtn.addEventListener("click", function(){
+    getComputerChoice();
+  pChoice = 'rock';
+  playRound(pChoice, computerSelection);
+})
+paperBtn.addEventListener("click", function(){
+    getComputerChoice();
+  pChoice = 'paper';
+  playRound(pChoice, computerSelection);
+})
+scissorBtn.addEventListener("click", function(){
+    getComputerChoice();
+  pChoice = 'scissor';
+  playRound(pChoice, computerSelection);
+})
 
-function playRound(playerChoice, computerSelection){
-    if(playerChoice == 'rock' && computerSelection =='scissor'){
-        console.log("Rock beats scissor, player Wins!");
+
+
+function playRound(pChoice, computerSelection){
+    if(pChoice == 'rock' && computerSelection =='scissor'){
+        txt.innerHTML="Rock beats scissor, player Wins!";
     }
-else if(playerChoice == 'paper' && computerSelection =='rock'){
-    console.log("Paper beats Rock, player Wins!");
+else if(pChoice == 'paper' && computerSelection =='rock'){
+    txt.innerHTML="Paper beats Rock, player Wins!";
 }
-else if(playerChoice == 'scissor' && computerSelection =='paper'){
-    console.log("Scissor beats Paper, player Wins!");
+else if(pChoice == 'scissor' && computerSelection =='paper'){
+    txt.innerHTML="Scissor beats Paper, player Wins!";
 }
-else if(playerChoice == 'scissor' && computerSelection =='rock'){
-    console.log("Rock beats scissor, Computer Wins!");
+else if(pChoice == 'scissor' && computerSelection =='rock'){
+    txt.innerHTML= "Rock beats scissor, Computer Wins!";
 }
-else if(playerChoice == 'rock' && computerSelection =='paper'){
-console.log("Paper beats Rock, computer Wins!");
+else if(pChoice == 'rock' && computerSelection =='paper'){
+txt.innerHTML="Paper beats Rock, computer Wins!";
 }
-else if(playerChoice == 'paper' && computerSelection =='scissor'){
-console.log("Scissor beats Paper, Computer Wins!");
+else if(pChoice == 'paper' && computerSelection =='scissor'){
+txt.innerHTML="Scissor beats Paper, Computer Wins!";
 }
-else if(playerChoice == computerSelection){
-    console.log(`Its a tie!`)
+else if(pChoice == computerSelection){
+    txt.innerHTML= `Its a tie!`;
 }
-else{
-    alert("Select proper Option");
-}
+
 }
 
 
-function playGame(){
-    for(i=1;i<=5;i++){
-        const playerChoice = prompt("Enter Your choice :Rock, Paper, or Scissor");
-        playRound(playerChoice,computerSelection);
-    }
-}
-playGame();
+// function playGame(){
+//         playRound(playerChoice,computerSelection);
+//     }
+// playGame();
